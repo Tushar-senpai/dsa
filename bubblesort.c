a71bf5a2e24272bf88331be1364d823f7842e4b0
Tushar-senpai <tk9471653832@gmail.com>
2025-07-05 17:14:01 +0530

    bubble sort

--- bubblesort.c
+++ bubblesort.c
@@ -1,68 +1,32 @@
 #include<stdio.h>
 
-
-void  bubblesort(int arr[],int size){
-int i,j,temp;
-for(i=0;i<size-1;i++){
-    for(j=0;j<size-1;j++){
+void bubblesort(int arr[],int size){
+    int i, j ,temp;
+    for(i=0;i<size-1;i++){
+      for(j=0;j<size-1-i;j++){
         if(arr[j]>arr[j+1]){
-                    
-        temp =arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1]=temp;
+           temp =arr[j];
+              arr[j]=arr[j+1];
+              arr[j+1]=temp;
+        }
+      }
+    }
+}
+
+void display(int arr[],int  n){
+    int i ;
+    for(int i=0;i<n;i++)
+    {
+        printf("%d  ,",arr[i]);
 
     }
 }
-}}
-
-void display(int arr[] ,int n){
-int i;
-for(i=0;i<n;i++){
-    printf("%d",arr[i]);
-
-}
-
-}
-
 
 int main(){
+int arr[] = {64, 34, 25, 12, 22, 11, 90};
+int n = sizeof(arr)/sizeof(arr[0]);
+bubblesort(arr, n);
+display(arr, n);
+return 0;
 
-   FILE *file;
-   int arr[100];
-   int n=0;
-   char ch;
-
-   file =fopen("input.txt","r");
-   if(file==NULL){
-    printf("file not found");
-    return 1;
-   }
-
-   else{
-    printf("file opend\n");
-   }
-
-//    ch = fgetc(file);
-//    printf("working\n");
-
-   while((ch =fgetc(file))!=EOF){
-    printf("%c",ch);
-if(ch>='0'&&ch<='9'){
-    arr[n]=ch-'0';
-    n++;
-}
-  
-   }
-   fclose(file);
-   
-   printf("\n");
-    
-display(arr,n);
-printf("\n");
-bubblesort(arr,n);
-printf("\n");
-display(arr,n);
-}
-
-
-
+} 
